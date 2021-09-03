f0ef3a187e20d535e0a25a2c83c2b787a68bea8f
Tom Durrant <tomchdurrant@gmail.com>
2021-09-03 13:32:22 +0100

    01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,13 +1,11 @@
 // Context Module Functions
 // http://localhost:3000/isolated/exercise/01.js
 
+import { dequal } from 'dequal'
 import * as React from 'react'
-import {dequal} from 'dequal'
-
+import { useAuth } from '../auth-context'
 // ./context/user-context.js
-
 import * as userClient from '../user-client'
-import {useAuth} from '../auth-context'
 
 const UserContext = React.createContext()
 UserContext.displayName = 'UserContext'
@@ -76,8 +74,15 @@ function useUser() {
 // 🐨 add a function here called `updateUser`
 // Then go down to the `handleSubmit` from `UserSettings` and put that logic in
 // this function. It should accept: dispatch, user, and updates
+function updateUser({dispatch, user, updates}) {
+  dispatch({type: 'start update', updates})
+  return userClient.updateUser(user, updates).then(
+    updatedUser => dispatch({type: 'finish update', updatedUser}),
+    error => dispatch({type: 'fail update', error}),
+  )
+}
 
-// export {UserProvider, useUser}
+// export {UserProvider, useUser. updateUser}
 
 // src/screens/user-profile.js
 // import {UserProvider, useUser} from './context/user-context'
@@ -98,11 +103,7 @@ function UserSettings() {
   function handleSubmit(event) {
     event.preventDefault()
     // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
+    updateUser({dispatch: userDispatch, user, updates: formState})
   }
 
   return (
--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,7 +4,7 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function Toggle() {
+function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
@@ -14,19 +14,31 @@ function Toggle() {
   // 💰 React.Children.map(props.children, child => {/* return child clone here */})
   // 📜 https://reactjs.org/docs/react-api.html#reactchildren
   // 📜 https://reactjs.org/docs/react-api.html#cloneelement
-  return <Switch on={on} onClick={toggle} />
+  return React.Children.map(children, (child, index) => {
+    return React.cloneElement(child, {
+      // onClick: toggle,
+      on: on,
+      toggle: toggle,
+    })
+  })
 }
 
 // 🐨 Flesh out each of these components
 
 // Accepts `on` and `children` props and returns `children` if `on` is true
-const ToggleOn = () => null
+const ToggleOn = ({on, children}) => {
+  return on ? children : null
+}
 
 // Accepts `on` and `children` props and returns `children` if `on` is false
-const ToggleOff = () => null
+const ToggleOff = ({on, children}) => {
+  return on ? null : children
+}
 
 // Accepts `on` and `toggle` props and returns the <Switch /> with those props.
-const ToggleButton = () => null
+const ToggleButton = ({on, toggle, ...props}) => {
+  return <Switch on={on} onClick={toggle} {...props} />
+}
 
 function App() {
   return (
